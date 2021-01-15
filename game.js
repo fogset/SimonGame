7621d9fb913937438e7c7b3bbe3e5d61b6f7825e
fogset <zhangth1990@hotmail.com>
2021-01-15 00:20:41 -0800

    add sound and animation when pressed

--- game.js
+++ game.js
@@ -1,9 +1,35 @@
 var gamePattern = [];
+var userClickedPattern  = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColour = buttonColours[nextSequence()];
 gamePattern.push(randomChosenColour);
+
+
 $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-function nextSequence(){
+
+$( ".btn" ).click(function() {
+  currentButton = this;
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(currentButton);
+
+});
+
+function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 3) + 1;
   return randomNumber;
+}
+
+function playSound(name) {
+  var currentPlayingSound = new Audio("sounds/" + name + ".mp3");
+  currentPlayingSound.play();
+}
+
+function animatePress(currentButton){
+  $(currentButton).addClass("pressed");
+  setTimeout(function(){
+            $(currentButton).removeClass("pressed");
+    }, 200);
 }
